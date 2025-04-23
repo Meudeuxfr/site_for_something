47e3e91fdd3be649685f30d7b7e0f2eb71018a2e
Meudeuxfr <meudeuxjr777@gmail.com>
2025-04-23 09:25:09 +0100

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -66,7 +66,7 @@ const Home: React.FC = () => {
         <h1 className="text-3xl font-bold text-center mb-10">Reserve a Table</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {tables.map((table) => (
-            <Table key={table.id} table={table} onReserve={reserveTable} />
+            <Table key={table.id} table={table} onReserve={() => reserveTable(table.id)} />
           ))}
         </div>
       </main>
